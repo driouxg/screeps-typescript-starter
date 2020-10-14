ae112fc364148a1efd6595756ae7ed9346978a02
Drioux Guidry <drioux_guidry@choicehotels.com>
2020-10-14 02:04:24 -0700

    Add cache check

--- src/construction/constructionHandler.ts
+++ src/construction/constructionHandler.ts
@@ -1,5 +1,5 @@
-import { buildStringGrid } from "utils/gridBuilder";
 import IConstructionHandler from "./IConstructionHandler";
+import { buildStringGrid } from "utils/gridBuilder";
 
 export default class ContructionHandler {
   private constructionHandlers: IConstructionHandler[];
@@ -13,40 +13,49 @@ export default class ContructionHandler {
       const room: Room = Game.rooms[roomName];
       let desiredState: string[][] = this.getCachedDesiredState(room);
 
-      // if (desiredState) return desiredState;
+      if (desiredState) {
+        this.print(room, desiredState);
+        return;
+      } else {
+        desiredState = buildStringGrid();
+      }
 
       for (const constructionHandler of this.constructionHandlers) {
         desiredState = constructionHandler.handle(room, desiredState);
       }
 
-      for (let y = 0; y < desiredState.length; y++) {
-        for (let x = 0; x < desiredState[y].length; x++) {
-          switch (desiredState[y][x]) {
-            case STRUCTURE_TOWER:
-              room.visual.text("T", x, y);
-              break;
-            case STRUCTURE_WALL:
-              room.visual.text("W", x, y);
-              break;
-            case STRUCTURE_ROAD:
-              room.visual.text("r", x, y);
-              break;
-            case STRUCTURE_RAMPART:
-              room.visual.text("R", x, y);
-              break;
-            default:
-              room.visual.text(".", x, y);
-              break;
-          }
-        }
-      }
+      this.print(room, desiredState);
 
       this.updateCachedDesiredState(room, desiredState);
     }
   }
 
+  private print(room: Room, desiredState: string[][]): void {
+    for (let y = 0; y < desiredState.length; y++) {
+      for (let x = 0; x < desiredState[y].length; x++) {
+        switch (desiredState[y][x]) {
+          case STRUCTURE_TOWER:
+            room.visual.text("T", x, y);
+            break;
+          case STRUCTURE_WALL:
+            room.visual.text("W", x, y);
+            break;
+          case STRUCTURE_ROAD:
+            room.visual.text("r", x, y);
+            break;
+          case STRUCTURE_RAMPART:
+            room.visual.text("R", x, y);
+            break;
+          default:
+            room.visual.text(".", x, y);
+            break;
+        }
+      }
+    }
+  }
+
   private getCachedDesiredState(room: Room): string[][] {
-    return room.memory.desiredState || buildStringGrid();
+    return room.memory.desiredState;
   }
 
   private updateCachedDesiredState(room: Room, desiredState: string[][]): void {
