c1828f63a210a83b5198ecb582387eeb5444f691
driouxg <driouxgd@yahoo.com>
2021-03-25 14:47:13 -0700

    Make repairer creeps prefer to supply towers over repairing manually

--- src/creeps/action/repairerHandler.ts
+++ src/creeps/action/repairerHandler.ts
@@ -1,6 +1,6 @@
 import CreepBehavior from "./commonCreepBehavior";
 import ICreepHandler from "./ICreepHandler";
-
+import { findTowers } from "../../utils/roomTowersUtil";
 export default class RepairerHandler implements ICreepHandler {
   private creepBehavior: CreepBehavior;
   private nextCreepHandler: ICreepHandler;
@@ -18,15 +18,34 @@ export default class RepairerHandler implements ICreepHandler {
   }
 
   private repair(creep: Creep): void {
-    const structures: AnyStructure[] = creep.room.find(FIND_STRUCTURES, {
-      filter: s => s.hits < s.hitsMax && s.structureType !== STRUCTURE_WALL
+    const towers = findTowers(creep.room).filter(tower => this.canSupplyTower(tower));
+
+    if (0 < towers.length) {
+      this.supplyTower(towers, creep);
+    } else {
+      this.repairStructure(creep);
+    }
+  }
+
+  private canSupplyTower(tower: StructureTower): boolean {
+    return tower && tower.energy < tower.energyCapacity;
+  }
+
+  private supplyTower(towers: StructureTower[], creep: Creep): void {
+    const tower = towers[0];
+
+    if (creep.transfer(tower, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE)
+      this.creepBehavior.moveToWithSinglePath(creep, tower.pos);
+  }
+
+  private repairStructure(creep: Creep): void {
+    const structures = creep.room.find(FIND_MY_STRUCTURES, {
+      filter: s => s.hits < s.hitsMax
     });
 
     if (0 < structures.length) {
       if (creep.repair(structures[0]) === ERR_NOT_IN_RANGE)
         this.creepBehavior.moveToWithSinglePath(creep, structures[0].pos);
-    } else {
-      this.nextCreepHandler.handle(creep);
-    }
+    } else this.nextCreepHandler.handle(creep);
   }
 }
