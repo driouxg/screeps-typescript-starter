bd248817829e2705d96a6871f8c37ae4e18915b6
driouxg <driouxgd@yahoo.com>
2021-03-26 14:26:48 -0700

    Make structure harvesters harvest from source as last resort

--- src/creeps/action/common/structureEnergyHarvester.ts
+++ src/creeps/action/common/structureEnergyHarvester.ts
@@ -26,6 +26,13 @@ export default class StructureEnergyCollector implements ICreepEnergyRetrieval {
       return;
     }
 
+    // harvest source
+    const source: Source | null = creep.pos.findClosestByPath(FIND_SOURCES_ACTIVE, {
+      filter: s => s.room === creep.room
+    });
+    if (!source) return;
+    if (creep.harvest(source) === ERR_NOT_IN_RANGE) creep.moveTo(source);
+
     // spawns
     // const spawns = findSpawns(creep.room).filter(s => 0 < s.store.getUsedCapacity(RESOURCE_ENERGY));
 
