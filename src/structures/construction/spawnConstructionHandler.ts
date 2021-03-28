50b16d0e5ad070c5d20dbc3b5ab9880342b2aa33
driouxg <driouxgd@yahoo.com>
2021-03-27 17:07:22 -0700

    Make spawn construction handler build the additional extra spawns

--- src/structures/construction/spawnConstructionHandler.ts
+++ src/structures/construction/spawnConstructionHandler.ts
@@ -2,14 +2,14 @@ import IConstructionHandler from "./IConstructionHandler";
 import { findNClosestEmptyPositionsLattice } from "../../utils/latticeSearch";
 
 export default class SpawnConstructionHandler implements IConstructionHandler {
-  private maxSpawnsPerRoom = 3;
+  private additionalSpawnsPerRoom = 2;
 
   public handle(room: Room, desiredState: string[][]): string[][] {
     if (!(room.controller && room.controller.my)) return desiredState;
     const positions: number[][] = findNClosestEmptyPositionsLattice(
       room.controller.pos,
       desiredState,
-      this.maxSpawnsPerRoom
+      this.additionalSpawnsPerRoom
     );
 
     for (const position of positions) {
