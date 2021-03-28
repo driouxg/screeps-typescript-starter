c8c97f0364ec6588bb99973d0308c2f7f797f4f7
driouxg <driouxgd@yahoo.com>
2021-03-28 11:44:24 -0700

    Refactor miner handler

--- src/creeps/action/minerHandler.ts
+++ src/creeps/action/minerHandler.ts
@@ -4,24 +4,23 @@ import PullRequestEvent from "room/pullRequestEvent";
 
 export default class MinerHandler implements ICreepHandler {
   handle(creep: Creep): void {
-    const sources = creep.room.find(FIND_SOURCES);
-    let isNextToSource = false;
-    let mineableSource = null;
-
-    for (const source of sources) {
-      if (creep.pos.isNearTo(source.pos)) {
-        isNextToSource = true;
-        mineableSource = source;
-        break;
-      }
-    }
-
-    if (isNextToSource) {
-      creep.harvest(mineableSource as Source);
+    const nearbyMineableSource = this.retrieveNearbyMineableSource(creep);
+    if (nearbyMineableSource) {
+      creep.harvest(nearbyMineableSource as Source);
       return;
     }
 
     this.sendPullRequestRoomEvent(creep);
+  }
+
+  private retrieveNearbyMineableSource(creep: Creep): Source | null {
+    const sources = creep.room.find(FIND_SOURCES);
+
+    for (const source of sources) {
+      if (creep.pos.isNearTo(source.pos)) return source;
+    }
+
+    return null;
   }
 
   private sendPullRequestRoomEvent(creep: Creep): void {
@@ -38,14 +37,21 @@ export default class MinerHandler implements ICreepHandler {
       for (const source of sources) {
         if (!source.pos.isNearTo(containerPos.x, containerPos.y)) continue;
 
-        const creeps = creep.room.lookForAt(LOOK_CREEPS, containerPos.x, containerPos.y).filter(c => c.my);
-
-        if (creeps.length === 1 && creeps[0].memory.role === creepRoles.MINER) continue;
+        if (this.sourceAlreadyHasMiner(creep.room, containerPos)) continue;
 
         return new RoomPosition(containerPos.x, containerPos.y, creep.room.name);
       }
     }
 
     return null;
+  }
+
+  private sourceAlreadyHasMiner(room: Room, containerPos: Position): boolean {
+    const creeps = room.lookForAt(LOOK_CREEPS, containerPos.x, containerPos.y).filter(c => c.my);
+
+    for (const creep of creeps) {
+      if (creep.memory.role === creepRoles.MINER) return true;
+    }
+    return false;
   }
 }
