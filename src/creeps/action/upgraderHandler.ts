d931b47c3bf4137f48aa458bed30204bc84929fa
driouxg <driouxgd@yahoo.com>
2021-03-28 14:02:18 -0700

    Change upgrader behavior

--- src/creeps/action/upgraderHandler.ts
+++ src/creeps/action/upgraderHandler.ts
@@ -1,27 +1,48 @@
-import CreepBehavior from "./common/creepBehavior";
+import PullRequestEvent from "room/pullRequestEvent";
 import ICreepHandler from "./ICreepHandler";
 
 export default class UpgraderHandler implements ICreepHandler {
-  private creepBehavior: CreepBehavior;
-
-  public constructor(creepBehavior: CreepBehavior) {
-    this.creepBehavior = creepBehavior;
-  }
-
   public handle(creep: Creep): void {
-    this.creepBehavior.updateWorkingState(creep);
-
-    if (this.creepBehavior.isWorking(creep)) this.upgradeController(creep);
-    else this.creepBehavior.harvestEnergy(creep);
-  }
-
-  private upgradeController(creep: Creep): void {
     const controller: StructureController | undefined = creep.room.controller;
 
     if (!controller) return;
 
-    if (creep.upgradeController(controller) === ERR_NOT_IN_RANGE) {
-      this.creepBehavior.moveToWithSinglePath(creep, controller.pos);
+    if (!creep.pos.isNearTo(controller.pos)) {
+      const controllerContainer = this.findControllerContainerPosition(creep, controller);
+      if (controllerContainer) creep.room.memory.events.push(new PullRequestEvent(controllerContainer, creep.name));
+      return;
     }
+
+    if (creep.store.energy === 0) {
+      this.collectEnergy(creep);
+    }
+
+    creep.upgradeController(controller);
+  }
+
+  private collectEnergy(creep: Creep): void {
+    const containers = creep.room
+      .lookForAt(LOOK_STRUCTURES, creep.pos)
+      .filter(s => s.structureType === STRUCTURE_CONTAINER);
+
+    if (containers) {
+      creep.withdraw(containers[0], RESOURCE_ENERGY);
+      return;
+    }
+
+    const energyPiles = creep.room.lookForAt(RESOURCE_ENERGY, creep.pos);
+
+    if (energyPiles) {
+      creep.pickup(energyPiles[0]);
+    }
+  }
+
+  private findControllerContainerPosition(creep: Creep, controller: StructureController): RoomPosition | null {
+    for (const containerPos of creep.room.memory.positions[STRUCTURE_CONTAINER]) {
+      if (controller.pos.isNearTo(containerPos.x, containerPos.y))
+        return new RoomPosition(containerPos.x, containerPos.y, creep.room.name);
+    }
+
+    return null;
   }
 }
