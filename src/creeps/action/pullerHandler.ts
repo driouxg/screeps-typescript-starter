b81b157d1e454a2f0e65f8549421a125da64a31a
driouxg <driouxgd@yahoo.com>
2021-03-29 08:50:30 -0700

    Suicide puller if no pull requests exist

--- src/creeps/action/pullerHandler.ts
+++ src/creeps/action/pullerHandler.ts
@@ -3,23 +3,31 @@ import ICreepHandler from "./ICreepHandler";
 
 export default class PullerHandler implements ICreepHandler {
   handle(creep: Creep): void {
-    for (const event of creep.room.memory.events) {
-      if (event.type !== "PULL_REQUEST") continue;
-
-      const pullRequest = event as PullRequestEvent;
-      const target = Game.creeps[pullRequest.creepId];
-
-      if (!target) continue;
-
-      const targetPos = new RoomPosition(pullRequest.destination.x, pullRequest.destination.y, target.room.name);
-      if (target.pos.isEqualTo(targetPos)) continue;
-
-      if (creep.pull(target) === ERR_NOT_IN_RANGE) creep.moveTo(target);
-      else {
-        target.move(creep);
-        if (creep.pos.isEqualTo(targetPos)) creep.move(creep.pos.getDirectionTo(target));
-        else creep.moveTo(targetPos);
-      }
+    const pullRequest = this.getFirstPullRequest(creep);
+    if (!pullRequest) {
+      creep.suicide();
+      return;
     }
+
+    const target = Game.creeps[pullRequest.creepId];
+
+    if (!target) return;
+
+    const targetPos = new RoomPosition(pullRequest.destination.x, pullRequest.destination.y, target.room.name);
+    if (target.pos.isEqualTo(targetPos)) return;
+
+    if (creep.pull(target) === ERR_NOT_IN_RANGE) creep.moveTo(target);
+    else {
+      target.move(creep);
+      if (creep.pos.isEqualTo(targetPos)) creep.move(creep.pos.getDirectionTo(target));
+      else creep.moveTo(targetPos);
+    }
+  }
+
+  private getFirstPullRequest(creep: Creep): PullRequestEvent | null {
+    for (const event of creep.room.memory.events) {
+      if (event.type === "PULL_REQUEST") return event as PullRequestEvent;
+    }
+    return null;
   }
 }
