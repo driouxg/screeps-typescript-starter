3e4594ae97bcc2cedf12d9233ba0e07259548073
driouxg <driouxgd@yahoo.com>
2021-04-02 07:52:59 -0700

    prefer offloading to container/container positions if less than 2000 energy is there instead of offloading to storage

--- src/creeps/action/haulerHandler.ts
+++ src/creeps/action/haulerHandler.ts
@@ -47,23 +47,32 @@ export default class HaulerHandler implements ICreepHandler {
         return this.creepBehavior.moveToWithSinglePath(creep, towers[0].pos);
     }
 
-    // offload to storage
-    const storage = findStorage(creep.room).filter(s => 0 < s.store.getFreeCapacity(RESOURCE_ENERGY));
-    if (0 < storage.length) {
-      if (creep.transfer(storage[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE)
-        return this.creepBehavior.moveToWithSinglePath(creep, storage[0].pos);
-    }
-
-    // offload to containers positions that are not next to sources
+    // offload to containers positions that are not next to sources until we reach max container amount
     const containerPositions = findCachedStructurePositions(creep.room, STRUCTURE_CONTAINER).filter(
       c => !this.isPositionNextToSource(creep, c)
     );
 
     if (0 < containerPositions.length) {
-      if (creep.pos.isEqualTo(containerPositions[0].x, containerPositions[0].y)) {
-        creep.drop(RESOURCE_ENERGY);
-        return this.creepEnergyRetrieval.retrieve(creep);
-      } else return this.creepBehavior.moveToWithSinglePath(creep, containerPositions[0]);
+      const pos = new RoomPosition(containerPositions[0].x, containerPositions[0].y, creep.room.name);
+      const containers = creep.room.lookForAt(LOOK_STRUCTURES, pos) as StructureContainer[];
+      const energyPiles = creep.room.lookForAt(LOOK_ENERGY, pos);
+
+      if (
+        (0 < containers.length && containers[0].store.energy < containers[0].store.getCapacity(RESOURCE_ENERGY)) ||
+        (0 < energyPiles.length && energyPiles[0].amount < 2000)
+      ) {
+        if (creep.pos.isEqualTo(pos.x, pos.y)) {
+          creep.drop(RESOURCE_ENERGY);
+          return this.creepEnergyRetrieval.retrieve(creep);
+        } else return this.creepBehavior.moveToWithSinglePath(creep, containerPositions[0]);
+      }
+    }
+
+    // offload to storage
+    const storage = findStorage(creep.room).filter(s => 0 < s.store.getFreeCapacity(RESOURCE_ENERGY));
+    if (0 < storage.length) {
+      if (creep.transfer(storage[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE)
+        return this.creepBehavior.moveToWithSinglePath(creep, storage[0].pos);
     }
 
     return ERR_NOT_FOUND;
