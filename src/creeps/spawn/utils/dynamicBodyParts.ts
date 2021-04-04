455c064e6a1d60e5a50c1bd82a54849d1614a126
driouxg <driouxgd@yahoo.com>
2021-04-04 11:36:43 -0700

    Refactor dynamic body part builder functions

--- src/creeps/spawn/utils/dynamicBodyParts.ts
+++ src/creeps/spawn/utils/dynamicBodyParts.ts
@@ -1,18 +1,21 @@
+const maxSpawnStore = 300;
+const maxNumBodyParts = 50;
+
 export function buildDynamicBodyParts(
   bluePrint: BodyPartConstant[],
   room: Room,
   minBluePrint?: BodyPartConstant[]
 ): BodyPartConstant[] {
-  return buildCappedBodyParts(bluePrint, room, 50, minBluePrint);
+  return buildCappedBodyParts(bluePrint, room, maxNumBodyParts, minBluePrint);
 }
 
 export function buildCappedBodyParts(
   bluePrint: BodyPartConstant[],
   room: Room,
-  cap: number,
+  numPartsCap: number,
   minBluePrint?: BodyPartConstant[]
 ): BodyPartConstant[] {
-  if (room.energyAvailable < 300) return [];
+  if (room.energyAvailable < maxSpawnStore) return [];
 
   bluePrint.sort((b1, b2) => BODYPART_COST[b1] - BODYPART_COST[b2]);
 
@@ -20,7 +23,7 @@ export function buildCappedBodyParts(
   let cost = minBluePrint ? minBluePrint.reduce((acc, val) => acc + BODYPART_COST[val], 0) : 0;
   let idx = 0;
 
-  while (parts.length < cap && cost + BODYPART_COST[bluePrint[idx]] <= room.energyAvailable) {
+  while (parts.length < numPartsCap && cost + BODYPART_COST[bluePrint[idx]] <= room.energyAvailable) {
     parts.push(bluePrint[idx]);
     cost += BODYPART_COST[bluePrint[idx]];
     idx = (idx + 1) % bluePrint.length;
